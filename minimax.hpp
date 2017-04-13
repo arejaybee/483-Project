ee3af8a5d84b06523baf76f23cbf9dfd596e503b
jon <ejno@users.noreply.github.com>
2017-04-12 20:07:40 -0400

    simplify

--- minimax.hpp
+++ minimax.hpp
@@ -12,8 +12,7 @@ public:
 
     virtual bool is_leaf() const = 0;
     virtual int get_node_score() const = 0;
-
-    virtual std::size_t get_number_of_children() const = 0;
+    virtual std::size_t get_child_count() const = 0;
 
     // will be freed upon destruction
     virtual MinimaxState *get_child(std::size_t index) = 0;
