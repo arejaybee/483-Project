8779f3a40a5a75cb01bd92934b2bcf0787940e05
jon <ejno@users.noreply.github.com>
2017-04-12 20:07:01 -0400

    ws

--- minimax.hpp
+++ minimax.hpp
@@ -7,16 +7,16 @@
 
 class MinimaxState
 {
-    public:
-        virtual ~MinimaxState();
+public:
+    virtual ~MinimaxState();
 
-        virtual bool is_leaf() const = 0;
-        virtual int get_node_score() const = 0;
+    virtual bool is_leaf() const = 0;
+    virtual int get_node_score() const = 0;
 
-        virtual std::size_t get_number_of_children() const = 0;
+    virtual std::size_t get_number_of_children() const = 0;
 
-        // will be freed upon destruction
-        virtual MinimaxState *get_child(std::size_t index) = 0;
+    // will be freed upon destruction
+    virtual MinimaxState *get_child(std::size_t index) = 0;
 };
 
 
