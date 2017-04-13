656e258481c1b4b45f475eb4ff6efff8e23938a5
arejaybee <arejaybee95@gmail.com>
2017-04-12 20:34:21 -0400

    Added some comments

--- minimax.hpp
+++ minimax.hpp
@@ -8,16 +8,18 @@
 class MinimaxState
 {
 public:
-    virtual ~MinimaxState();
-
-    virtual int get_node_score() const = 0;
-    virtual std::size_t get_child_count() const = 0;
-
-    virtual MinimaxState &get_child(std::size_t index) = 0;
+  //deconstructor
+  virtual ~MinimaxState();
+  
+  //the value of a node
+  virtual int get_node_score() const = 0;
+  //number of children a node has
+  virtual std::size_t get_child_count() const = 0;
+  
+  //should return a child node given some index
+  virtual MinimaxState &get_child(std::size_t index) = 0;
 };
 
-
 const MinimaxState &minimax_evaluate(MinimaxState &head);
-
 
 #endif
