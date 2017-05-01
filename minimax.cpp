8c6eb08f9eedcdd67ee10c06708ac2ff4e38ee61
tmaher1 <tmaher1@gmail.com>
2017-05-01 19:19:57 -0400

    cleaned up minimax indentation

--- minimax.cpp
+++ minimax.cpp
@@ -9,53 +9,40 @@ MinimaxState::~MinimaxState()
 }
 
 
+// currently implements a very basic Minimax function (recursive; no pruning)
 std::pair<std::size_t, int> minimax_evaluate(MinimaxState &head)
 {
-    // currently implements a very basic Minimax function (recursive; no pruning)
+	const std::size_t child_count = head.get_child_count();
+	std::pair<std::size_t, int> result(0, 0);
 
-    const std::size_t child_count = head.get_child_count();
+	if (child_count == 0) {
+		throw std::invalid_argument("head is a leaf");
+	}
 
-    if (child_count == 0)
-      {
-	throw std::invalid_argument("head is a leaf");
-      }
-
-    std::pair<std::size_t, int> result(0, 0);
-
-    //check all children of the node
-    for (std::size_t i = 0; i < child_count; ++i) 
-      {
-	MinimaxState &child = head.get_child(i);
-	int candidate;
+  for (std::size_t i = 0; i < child_count; ++i) { //check all children of the node
+		MinimaxState &child = head.get_child(i);
+		int candidate;
 	
-	//if there are no children, calculate the score
-	if (child.get_child_count() == 0){
-	    candidate = child.get_node_score();
-	  }
-	//otherwize recurse down(im not sure what second is?)
-	else{
-	    candidate = minimax_evaluate(child).second;
-	  }
-	//if the first iteration of loop, assume it is the result
-	if (i == 0) {
+		if (child.get_child_count() == 0) { //if there are no children, calculate the score
+	  	candidate = child.get_node_score();
+		}
+		else { //otherwize recurse down(im not sure what second is?)
+			candidate = minimax_evaluate(child).second;
+		}
+		if (i == 0) { //if the first iteration of loop, assume it is the result
 	    result.first = i;
 	    result.second = candidate;
 	  }
-	//if we are looking for the min
-	if (head.get_player() == MinimaxState::PLAYER_MIN) {
-	  //see if the current node's number is less than the result
-	  //if so, replace
-	    if (candidate < result.second) {
+		if (head.get_player() == MinimaxState::PLAYER_MIN) { //if we are looking for the min
+	    if (candidate < result.second) { //see if the current node's number is less than the result. if so, replace
 	      result.first = i;
 	      result.second = candidate;
 	    }
 	  } 
-	//otherwise do the same for the max
-	else if (candidate > result.second) {
-	  result.first = i;
-	  result.second = candidate;
-	}
-    }
-
-    return result;
+		else if (candidate > result.second) { //otherwise do the same for the max
+			result.first = i;
+			result.second = candidate;
+		}
+  }
+	return result;
 }
--- minimax.hpp
+++ minimax.hpp
@@ -7,23 +7,23 @@
 
 class MinimaxState
 {
-public:
-    enum Player {PLAYER_MIN, PLAYER_MAX};
+    public:
+        enum Player {PLAYER_MIN, PLAYER_MAX};
 
-    // deconstructor
-    virtual ~MinimaxState();
+        // deconstructor
+        virtual ~MinimaxState();
 
-    // the player at a node
-    virtual Player get_player() const = 0;
+        // the player at a node
+        virtual Player get_player() const = 0;
 
-    // the value of a node
-    virtual int get_node_score() const = 0;
+        // the value of a node
+        virtual int get_node_score() const = 0;
 
-    // number of children a node has
-    virtual std::size_t get_child_count() const = 0;
+        // number of children a node has
+        virtual std::size_t get_child_count() const = 0;
 
-    // should return a child node given some index
-    virtual MinimaxState &get_child(std::size_t index) = 0;
+        // should return a child node given some index
+        virtual MinimaxState &get_child(std::size_t index) = 0;
 };
 
 
