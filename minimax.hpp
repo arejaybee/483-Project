266dbfd3d0f9a9143dddabb6417ad7269c70f794
jon <ejno@users.noreply.github.com>
2017-05-03 19:47:38 -0400

    work on minimax

--- minimax.hpp
+++ minimax.hpp
@@ -2,33 +2,10 @@
 #define MINIMAX_HPP
 
 
-#include <utility>
+#include "GameState.h"
 
 
-class MinimaxState
-{
-    public:
-        enum Player {PLAYER_MIN, PLAYER_MAX};
-
-        // deconstructor
-        virtual ~MinimaxState();
-
-        // the player at a node
-        virtual Player get_player() const = 0;
-
-        // the value of a node
-        virtual int get_node_score() const = 0;
-
-        // number of children a node has
-        virtual std::size_t get_child_count() const = 0;
-
-        // should return a child node given some index
-        virtual MinimaxState &get_child(std::size_t index) = 0;
-};
-
-
-// returns <index of optimal child, score>
-std::pair<std::size_t, int> minimax_evaluate(MinimaxState &head);
+std::size_t minimax(const GameState &node, std::size_t max_depth);
 
 
 #endif
