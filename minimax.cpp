44c3d6fd6ea310e97990fe7995c85d4d6382defd
jon <ejno@users.noreply.github.com>
2017-05-03 21:27:42 -0400

    use GameState::getPotentialChildren()

--- minimax.cpp
+++ minimax.cpp
@@ -8,14 +8,15 @@ namespace
 
 
 static int
-minimax_evaluate(const GameState &node, const std::size_t depth, int alpha, int beta)
+minimax_evaluate(const GameState &node, const std::size_t depth, int alpha,
+        int beta)
 {
     if (depth == 0)
         return node.evaluateScore();
 
-    const std::vector<Move> moves = node.getPotentialMoves();
+    const std::vector<GameState> children = node.getPotentialChildren();
 
-    if (moves.empty())
+    if (children.empty())
         return node.evaluateScore();
 
     const Piece turn = node.getTurn();
@@ -23,11 +24,9 @@ minimax_evaluate(const GameState &node, const std::size_t depth, int alpha, int 
     if (turn == PIECE_X) {
         int v = std::numeric_limits<int>::min();
 
-        for (std::vector<Move>::const_iterator it = moves.begin(); it != moves.end();
-                ++it) {
-            GameState child = node;
-            child.changeBoardPiece(*it, turn);
-            v = std::max(v, minimax_evaluate(child, depth - 1, alpha, beta));
+        for (std::vector<GameState>::const_iterator it = children.begin();
+                it != children.end(); ++it) {
+            v = std::max(v, minimax_evaluate(*it, depth - 1, alpha, beta));
             alpha = std::max(alpha, v);
 
             if (beta <= alpha)
@@ -38,11 +37,9 @@ minimax_evaluate(const GameState &node, const std::size_t depth, int alpha, int 
     } else {
         int v = std::numeric_limits<int>::min();
 
-        for (std::vector<Move>::const_iterator it = moves.begin(); it != moves.end();
-                ++it) {
-            GameState child = node;
-            child.changeBoardPiece(*it, turn);
-            v = std::min(v, minimax_evaluate(child, depth - 1, alpha, beta));
+        for (std::vector<GameState>::const_iterator it = children.begin();
+                it != children.end(); ++it) {
+            v = std::min(v, minimax_evaluate(*it, depth - 1, alpha, beta));
             beta = std::min(beta, v);
 
             if (beta <= alpha)
@@ -61,23 +58,21 @@ Move
 minimax(const GameState &node, std::size_t max_depth)
 {
     const Piece turn = node.getTurn();
-    const std::vector<Move> moves = node.getPotentialMoves();
+    const std::vector<GameState> children = node.getPotentialChildren();
     std::pair<Move, int> candidate(Move(), std::numeric_limits<int>::min());
 
     if (turn == PIECE_O)
         candidate.second = std::numeric_limits<int>::max();
 
-    for (std::vector<Move>::const_iterator it = moves.begin(); it != moves.end();
-            ++it) {
-        GameState child = node;
-        child.changeBoardPiece(*it, node.getTurn());
-        const int v = minimax_evaluate(child, max_depth - 1,
+    for (std::vector<GameState>::const_iterator it = children.begin();
+            it != children.end(); ++it) {
+        const int v = minimax_evaluate(*it, max_depth - 1,
                 std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
 
         if (turn == PIECE_X && v > candidate.second)
-            candidate = std::make_pair(*it, v);
+            candidate = std::make_pair(it->lastMove, v);
         else if (turn == PIECE_O && v < candidate.second)
-            candidate = std::make_pair(*it, v);
+            candidate = std::make_pair(it->lastMove, v);
     }
 
     return candidate.first;
