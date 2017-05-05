d3ee73e0df90bf134ff80bdf6235b73db3b8b6a5
jon <ejno@users.noreply.github.com>
2017-05-05 17:36:55 -0400

    fixes

--- minimax.cpp
+++ minimax.cpp
@@ -9,22 +9,20 @@ namespace
 
 
 static int
-negamax(const GameState &node, const std::size_t depth, int alpha, int beta, const bool my_turn)
+negamax(const GameState &node, const std::size_t depth, int alpha, int beta)
 {
-    const int color = my_turn ? 1 : -1;
-
     if (depth == 0)
-        return color * node.evaluateScore(node.getTurn());
+        return node.evaluateScore(node.getTurn());
 
     const std::vector<GameState> children = node.getPotentialChildren();
 
     if (children.empty())
-        return color * node.evaluateScore(node.getTurn());
+        return node.evaluateScore(node.getTurn());
 
     int best_value = std::numeric_limits<int>::min() + 1;
 
     for (std::vector<GameState>::const_iterator it = children.begin(); it != children.end(); ++it) {
-        const int v = -negamax(*it, depth - 1, -beta, -alpha, !my_turn);
+        const int v = -negamax(*it, depth - 1, -beta, -alpha);
 
         best_value = std::max(best_value, v);
         alpha = std::max(alpha, v);
@@ -49,7 +47,7 @@ minimax(const GameState &node, std::size_t max_depth)
     std::pair<Move, int> candidate(Move(), std::numeric_limits<int>::min() + 1);
 
     for (std::vector<GameState>::const_iterator it = children.begin(); it != children.end(); ++it) {
-        const int v = negamax(*it, max_depth, std::numeric_limits<int>::min() + 1, std::numeric_limits<int>::max() - 1, true);
+        const int v = negamax(*it, max_depth, std::numeric_limits<int>::min() + 1, std::numeric_limits<int>::max() - 1);
 
         if (v > candidate.second) {
             candidate.second = v;
