cd8fbd33fcc4e4afe765d9c845ba911cb11803c0
jon <ejno@users.noreply.github.com>
2017-05-05 17:11:47 -0400

    fix

--- minimax.cpp
+++ minimax.cpp
@@ -9,9 +9,9 @@ namespace
 
 
 static int
-negamax(const GameState &node, const std::size_t depth, int alpha, int beta, const bool color_flag)
+negamax(const GameState &node, const std::size_t depth, int alpha, int beta, const bool my_turn)
 {
-    const int color = 1 ? color_flag : -1;
+    const int color = my_turn ? 1 : -1;
 
     if (depth == 0)
         return color * node.evaluateScore(node.getTurn());
@@ -21,10 +21,10 @@ negamax(const GameState &node, const std::size_t depth, int alpha, int beta, con
     if (children.empty())
         return color * node.evaluateScore(node.getTurn());
 
-    int best_value = std::numeric_limits<int>::min();
+    int best_value = std::numeric_limits<int>::min() + 1;
 
     for (std::vector<GameState>::const_iterator it = children.begin(); it != children.end(); ++it) {
-        const int v = -negamax(*it, depth - 1, -beta, -alpha, !color_flag);
+        const int v = -negamax(*it, depth - 1, -beta, -alpha, !my_turn);
 
         best_value = std::max(best_value, v);
         alpha = std::max(alpha, v);
@@ -46,10 +46,10 @@ minimax(const GameState &node, std::size_t max_depth)
     // get the children
     const std::vector<GameState> children = node.getPotentialChildren();
 
-    std::pair<Move, int> candidate(Move(), std::numeric_limits<int>::min());
+    std::pair<Move, int> candidate(Move(), std::numeric_limits<int>::min() + 1);
 
     for (std::vector<GameState>::const_iterator it = children.begin(); it != children.end(); ++it) {
-        const int v = negamax(*it, max_depth, std::numeric_limits<int>::min(), std::numeric_limits<int>::max(), true);
+        const int v = negamax(*it, max_depth, std::numeric_limits<int>::min() + 1, std::numeric_limits<int>::max() - 1, true);
 
         if (v > candidate.second) {
             candidate.second = v;
@@ -57,7 +57,7 @@ minimax(const GameState &node, std::size_t max_depth)
         }
     }
 
-    if (candidate.second == std::numeric_limits<int>::min())
+    if (candidate.second == std::numeric_limits<int>::min() + 1)
         throw std::logic_error("did not find any moves");
 
     return candidate.first;
