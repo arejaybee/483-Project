80a90dd483c1005d997cb051e96110c359240205
jon <ejno@users.noreply.github.com>
2017-05-06 20:48:21 -0400

    formatting

--- minimax.cpp
+++ minimax.cpp
@@ -2,43 +2,46 @@
 
 #include <limits>
 #include <stdexcept>
+
 #include <omp.h>
 
-const int NUM_THREADS = 2;
+
+const std::size_t NUM_THREADS = 2;
+
 
 Piece p;
 namespace
 {
 
+
 static int
 negamax(const GameState &node, const std::size_t depth, int alpha,
         const int beta)
 {
-  //at the bottom of the tree, evaluate their score
+    // at the bottom of the tree, evaluate their score
     if (depth == 0)
         return node.evaluateScore(p);
-    //get the children of this node
+
+    // get the children of this node
     const std::vector<GameState> children = node.getPotentialChildren();
-    //if there are no children, just evaluate
+
+    // if there are no children, just evaluate
     if (children.empty())
         return node.evaluateScore(p);
-    
-    int best_value = std::numeric_limits<int>::min() + 1;
-    
-    //loop through the children
-    /*for (std::vector<GameState>::const_iterator it = children.begin();
-      it != children.end(); ++it) {*/
-      omp_set_dynamic(0);
-      omp_set_num_threads(NUM_THREADS);
-      unsigned int i;
-#pragma omp parallel for private(i) num_threads(NUM_THREADS)
-    for (i = 0; i < children.size(); i++) {
-        if (alpha < beta) {
-        const int v = -negamax(children.at(i),depth - 1, -beta, -alpha);
 
-        // update best_value to be the highest scoring child by the end of this loop
-        best_value = std::max(best_value, v);
-        alpha = std::max(alpha, v);
+    int best_value = std::numeric_limits<int>::min() + 1;
+
+    // loop through the children
+    omp_set_dynamic(0);
+    omp_set_num_threads(NUM_THREADS);
+#pragma omp parallel for num_threads(NUM_THREADS)
+    for (std::size_t i = 0; i < children.size(); ++i) {
+        if (alpha < beta) {
+            const int v = -negamax(children.at(i), depth - 1, -beta, -alpha);
+
+            // update best_value to be the highest scoring child by the end of this loop
+            best_value = std::max(best_value, v);
+            alpha = std::max(alpha, v);
         }
     }
 
@@ -56,25 +59,25 @@ minimax(const GameState &node, std::size_t max_depth)
     const std::vector<GameState> children = node.getPotentialChildren();
 
     p = node.getTurn();
-    
-    //variable for the optimal move
+
+    // variable for the optimal move
     std::pair<Move, int> candidate(Move(), std::numeric_limits<int>::min() + 1);
 
-    //loop through all of this gamestates children
+    // loop through all of this gamestates children
     for (std::vector<GameState>::const_iterator it = children.begin();
             it != children.end(); ++it) {
-      //each child calls the min/max tree to get a score
+        // each child calls the min/max tree to get a score
         const int v = negamax(*it, max_depth, std::numeric_limits<int>::min() + 1,
                 std::numeric_limits<int>::max() - 1);
-	
-	// pick the highest score as the correct choice
+
+        // pick the highest score as the correct choice
         if (v > candidate.second) {
             candidate.second = v;
             candidate.first = it->lastMove;
         }
     }
 
-    //if there was no move, there is an error (this function shouldn't have been called)
+    // if there was no move, there is an error (this function shouldn't have been called)
     if (candidate.second == std::numeric_limits<int>::min() + 1)
         throw std::logic_error("did not find any moves");
 
